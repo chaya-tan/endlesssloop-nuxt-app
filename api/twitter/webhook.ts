4b44bda22eb191caf594cfad2f7b4f5aadb39857
chaya-tan <chayatan.ang@gmail.com>
2022-02-23 01:08:18 +0700

    changed router endpoint

--- api/twitter/webhook.ts
+++ api/twitter/webhook.ts
@@ -1,9 +1,9 @@
 import { Router, Request, Response } from 'express'
 const router = Router();
 
-router.use("/hello", (req: Request, res: Response) => {
+router.post("/twitter/webhook", (req: Request, res: Response) => {
     console.log("request: ", req);
-    res.end('Hello world!')
+    res.send({ status: 200 })
 })
 
 export default router
