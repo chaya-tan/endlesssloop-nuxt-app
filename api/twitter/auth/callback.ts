324e54b99386d73aad00d01eb0ab941f36a01c72
chaya-tan <chayatan.ang@gmail.com>
2022-03-01 18:19:45 +0700

    added callback endpoint for twitter access token request

--- api/twitter/auth/callback.ts
+++ api/twitter/auth/callback.ts
@@ -6,7 +6,7 @@ const path = "/twitter/auth/callback"
 
 router.get(path, (req: Request, res: Response) => {
     console.log("request: ", req);
-    res.status(200)
+    res.status(200).send(req.query)
 })
 
 router.post(path, (req: Request, res: Response) => {
