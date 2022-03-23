7acfcfc3cbd124a39fafaf59bc269d936d95e9d8
chaya-tan <chayatan.ang@gmail.com>
2022-03-23 20:11:30 +0700

    added line bot sdk

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
 import TwitterWebhook from './twitter/webhook'
 import TwitterRequestToken from './twitter/auth/requesttoken'
 import TwitterAuthCallback from './twitter/auth/callback'
@@ -8,12 +10,25 @@ import LineWebhook from './line/webhook'
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(TwitterWebhook)
 app.use(TwitterRequestToken)
 app.use(TwitterAuthCallback)
 app.use(TwitterUserShow)
 app.use(LineWebhook)
 app.use(Telegram)
+
+
+
+app.post('/requests', (req, res) => {
+  console.log(req.body);
+  res.status(201).json('all ok');
+})
 
 if (require.main === module) {
   const port = 3001
--- api/line/webhook.ts
+++ api/line/webhook.ts
@@ -1,25 +1,26 @@
 import { Router, Request, Response } from 'express'
+import { validateSignature as lineBotValidateSignature, WebhookRequestBody } from "@line/bot-sdk"
+import crypto from 'crypto'
 
 const router = Router();
 
 const path = "/line/webhook"
 
-const lineChannelSecret = process.env.LINE_CHANNEL_SECRET
+const lineConfig = {
+  chennelAcessToken: process.env.LINE_CHANNEL_ACESS_TOKEN || "",
+  channelSecret: process.env.LINE_CHANNEL_SECRET || ""
+}
 
-router.post(path, (req: Request, res: Response) => {
-    console.log("Req Body: ", req.body);
-    console.log("Req Header: ", req.header);
-    console.log("lineChannelSecret", lineChannelSecret)
+router.post(path, (req: Request<{}, {}, WebhookRequestBody>, res: Response) => {
+    const lineSignature = crypto.createHmac('SHA256', lineConfig.channelSecret).update(JSON.stringify(req.body)).digest('base64');
+    const isValidated = lineBotValidateSignature(JSON.stringify(req.body), lineConfig.channelSecret, lineSignature)
+
+    if(isValidated) {
+      const allMessageEvents = req.body.events.filter(event => event.type === "message")
+      console.log(`Total Message recieved: ${allMessageEvents.length}`)
+    }
+
     res.status(200).send({success: true})
 })
 
 export default router
-
-// const crypto = require('crypto');
-
-// const channelSecret = '...'; // Channel secret string
-// const body = '...'; // Request body string
-// const signature = crypto
-//   .createHmac('SHA256', channelSecret)
-//   .update(body).digest('base64');
-// // Compare x-line-signature request header and the signature
