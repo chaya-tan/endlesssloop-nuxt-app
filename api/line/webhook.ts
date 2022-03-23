12ce4ab2f3c9cc24e5ef3ee87d55b7f015dbeea3
chaya-tan <chayatan.ang@gmail.com>
2022-03-23 17:32:33 +0700

    added endpoint for line webhookt

--- api/line/webhook.ts
+++ api/line/webhook.ts
@@ -4,9 +4,21 @@ const router = Router();
 
 const path = "/line/webhook"
 
+const lineChannelSecret = process.env.LINE_CHANNEL_SECRET
+
 router.post(path, (req: Request, res: Response) => {
     console.log("request: ", req);
+    console.log("lineChannelSecret", lineChannelSecret)
     res.status(200).send({success: true})
 })
 
 export default router
+
+// const crypto = require('crypto');
+
+// const channelSecret = '...'; // Channel secret string
+// const body = '...'; // Request body string
+// const signature = crypto
+//   .createHmac('SHA256', channelSecret)
+//   .update(body).digest('base64');
+// // Compare x-line-signature request header and the signature
